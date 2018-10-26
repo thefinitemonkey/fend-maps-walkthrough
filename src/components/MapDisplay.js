78f222cdb9ebabe3abee6cbcb9c7c9efec53ad22
Doug Brown <doug.brown@academy.com>
2018-10-26 10:58:28 -0500

    Add basic marker display and data management

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 
 const MAP_KEY = "AIzaSyBSf2q0a4Umr65w17nKsfLOl6L99Vj2DsQ";
 
 class MapDisplay extends Component {
     state = {
-        map: null
+        map: null,
+        markers: [],
+        markerProps: [],
+        activeMarker: null,
+        activeMarkerProps: null,
+        showingInfoWindow: false
     };
 
     componentDidMount = () => {
@@ -14,6 +19,65 @@ class MapDisplay extends Component {
     mapReady = (props, map) => {
         // Save the map reference in state and prepare the location markers
         this.setState({map});
+        this.updateMarkers(this.props.locations);
+    }
+
+    closeInfoWindow = () => {
+        // Disable any active marker animation
+        this.state.activeMarker && this
+            .state
+            .activeMarker
+            .setAnimation(null);
+        this.setState({showingInfoWindow: false, activeMarker: null, activeMarkerProps: null});
+    }
+
+    onMarkerClick = (props, marker, e) => {
+        // Close any info window already open
+        this.closeInfoWindow();
+
+        // Set the state to have the marker info show
+        this.setState({showingInfoWindow: true, activeMarker: marker, activeMarkerProps: props});
+    }
+
+    updateMarkers = (locations) => {
+        // If all the locations have been filtered then we're done
+        if (!locations) 
+            return;
+        
+        // For any existing markers remove them from the map
+        this
+            .state
+            .markers
+            .forEach(marker => marker.setMap(null));
+
+        // Iterate over the locations to create parallel references to marker properties
+        // and the markers themselves that can be used for reference in interactions.
+        // Add the markers to the map along the way.
+        let markerProps = [];
+        console.log("map: ", this.state.map);
+        let markers = locations.map((location, index) => {
+            let mProps = {
+                key: index,
+                index,
+                name: location.name,
+                position: location.pos,
+                url: location.url
+            };
+            markerProps.push(mProps);
+
+            let animation = this.props.google.maps.Animation.DROP;
+            let marker = new this.props.google.maps.Marker({
+                position: location.pos, 
+                map: this.state.map, 
+                animation
+            });
+            marker.addListener('click', () => {
+                this.onMarkerClick(mProps, marker, null);
+            });
+            return marker;
+        })
+
+        this.setState({markers, markerProps});
     }
 
     render = () => {
@@ -25,6 +89,7 @@ class MapDisplay extends Component {
             lat: this.props.lat,
             lng: this.props.lon
         }
+        let amProps = this.state.activeMarkerProps;
 
         return (
             <Map
@@ -36,6 +101,20 @@ class MapDisplay extends Component {
                 style={style}
                 initialCenter={center}
                 onClick={this.closeInfoWindow}>
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onClose={this.closeInfoWindow}>
+                    <div>
+                        <h3>{amProps && amProps.name}</h3>
+                        {amProps && amProps.url
+                            ? (
+                                <a href={amProps.url}>See website</a>
+                            )
+                            : ""}
+                        }
+                    </div>
+                </InfoWindow>
             </Map>
         )
     }
