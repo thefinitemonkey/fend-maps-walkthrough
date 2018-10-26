5c612cfa5fd85f458a132778b4565d424add480f
Doug Brown <doug.brown@academy.com>
2018-10-26 11:18:47 -0500

    Add list drawer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import React, {Component} from 'react';
 import './App.css';
 import locations from './data/locations.json';
 import MapDisplay from './components/MapDisplay';
+import ListDrawer from './components/ListDrawer';
 
 class App extends Component {
   state = {
     lat: 29.7844913,
     lon: -95.7800231,
     zoom: 13,
-    all: locations
+    all: locations,
+    open: false
+  }
+
+  styles = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: "absolute",
+      left: 10,
+      top: 20,
+      background: "white",
+      padding: 10
+    },
+    hide: {
+      display: 'none'
+    },
+    header: {
+      marginTop: "0px"
+    }
+  };
+
+  toggleDrawer = () => {
+    // Toggle the value controlling whether the drawer is displayed
+    this.setState({
+      open: !this.state.open
+    });
   }
 
   render = () => {
     return (
       <div className="App">
         <div>
+          <button onClick={this.toggleDrawer} style={this.styles.menuButton}>
+            <i className="fa fa-bars"></i>
+          </button>
           <h1>Katy, TX Mexican Restaurants</h1>
         </div>
         <MapDisplay
@@ -22,6 +52,12 @@ class App extends Component {
           lon={this.state.lon}
           zoom={this.state.zoom}
           locations={this.state.all}/>
+        <ListDrawer
+          locations={this.state.all}
+          open={this.state.open}
+          toggleDrawer={this.toggleDrawer}
+          filterLocations={this.updateQuery}
+          clickListItem={this.clickListItem}/>
       </div>
     );
   }
---
+++ src/components/ListDrawer.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+
+class ListDrawer extends Component {
+    state = {
+        open: false,
+        query: ""
+    }
+
+    styles = {
+        list: {
+            width: "250px",
+            padding: "0px 15px 0px"
+        },
+        noBullets: {
+            listStyleType: "none",
+            padding: 0
+        },
+        fullList: {
+            width: 'auto'
+        },
+        listItem: {
+            marginBottom: "15px"
+        },
+        listLink: {
+            background: "transparent",
+            border: "none",
+            color: "black"
+        },
+        filterEntry: {
+            border: "1px solid gray",
+            padding: "3px",
+            margin: "30px 0px 10px",
+            width: "100%"
+        }
+    };
+
+    updateQuery = (newQuery) => {
+        // Save the new query string in state and pass the string
+        // up the call tree
+        this.setState({ query: newQuery });
+    }
+
+    render = () => {
+        return (
+            <div>
+                <Drawer open={this.props.open} onClose={this.props.toggleDrawer}>
+                    <div style={this.styles.list}>
+                        <input
+                            style={this.styles.filterEntry}
+                            type="text"
+                            placeholder="Filter list"
+                            name="filter"
+                            onChange={e => this
+                                .updateQuery(e.target.value)}
+                            value={this.state.query} />
+                        <ul style={this.styles.noBullets}>
+                            {this.props.locations && this
+                                .props
+                                .locations
+                                .map((location, index) => {
+                                    return (
+                                        <li style={this.styles.listItem} key={index}>
+                                            <button style={this.styles.listLink} key={index} onClick={e => this.props.clickListItem(index)}>{location.name}</button>
+                                        </li>
+                                    )
+                                })}
+                        </ul>
+                    </div>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+export default ListDrawer;
